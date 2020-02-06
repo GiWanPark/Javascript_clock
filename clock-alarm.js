6945975e2dba31f1a0690aede3dc0a18ab615e85
Giwan Park <aaamy91@naver.com>
2020-02-06 23:12:53 +0000

    Update clock-alarm.js

--- clock-alarm.js
+++ clock-alarm.js
@@ -1,2 +1,17 @@
-const clock_area = document.querySelector("clock-alarm");
-const 
+const clock_area = document.querySelector(".clock-alarm");
+const btn = clock_area.querySelector("#startButton");
+const concentrate = clock_area.querySelector("#concentrate-minute");
+const brk = clock_area.querySelector("#break-minute");
+
+function handleOnclick(event)
+{
+    
+}
+
+function init()
+{
+    btn.addEventListener("onclick", handleOnclick);
+}
+
+
+init();
